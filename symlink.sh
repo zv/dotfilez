4e39b4283cafaddcf8ab6c273a1efbe2560e7c40
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2012-05-11 16:55:25 -0700

    updated this here symbolic linking script (pretty elite, I know)

---
+++ symlink.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+# Some pretty simple shit right here
+
+git pull
+
+ln -sf .vimrc ~/.vimrc
+ln -sf .gitconfig ~/.gitconfig
+ln -sf .gitignore ~/.gitignore
+ln -sf ./vim ~/.vim
+ln -sf .vimrc ~/.vimrc
+ln -sf .irbrc ~/.irbrc
+ln -sf .erlang ~/.erlang
