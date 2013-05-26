db2b8843e2c6180270047be2bfb9608146fb6400
Zephyr <zephyr.pellerin@gmail.com>
2013-05-26 14:27:08 -0700

    xmonad mod-x kills xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,18 +7,26 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import qualified Data.Map as M
+
+-- import XMonad.Layout.NoBorders
+
 import System.IO
+
+mykeys (XConfig {modMask = modm}) = M.fromList $ 
+  [ ((modm , xK_x), sendMessage ToggleStruts) ]
 
 main = do
      xmproc  <- spawnPipe "/usr/bin/xmobar" 
      session <- getEnv "DESKTOP_SESSION"
-     -- one day we will have locale support in uxterm
      xmonad $ (maybe desktopConfig desktop session) { terminal = "urxvt"
                                                     , logHook = dynamicLogWithPP sjanssenPP 
                                                         { ppOutput = hPutStrLn xmproc
                                                         , ppTitle = xmobarColor "black" "" . shorten 50
                                                         }
+                                                     , layoutHook=avoidStruts $ layoutHook defaultConfig
+                                                     , manageHook=manageHook defaultConfig <+> manageDocks
+                                                     , keys = \c -> mykeys c `M.union` keys defaultConfig c
                                                     , focusedBorderColor = "#FF0000" 
                                                     }
-
 desktop _ = desktopConfig
