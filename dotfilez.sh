1c9b94a623754ed07bba453fd54a2a9a5413b8bc
zv <zv@nxvr.org>
2014-01-13 23:02:25 -0800

    fixed inexplicable dotfilez.sh

--- dotfilez.sh
+++ dotfilez.sh
@@ -8,14 +8,14 @@ if [ "$1" == "install" ]; then
     git ls-tree --name-only HEAD | \
         grep -v '^\.\|Makefile\|README.md\|id_rsa.gpg\|ssh_config' | \
         xargs -p -I % sh -c "ln -s $(realpath %) $HOME/.%"
+fi
 
-else if [ "$1" == "backup" ]; then
+if [ "$1" == "backup" ]; then
     mkdir -p ~/.dotfilez_backups/`date %+F`
     ls $(DOTFILES) | xargs -n 1 readlink -f | \
         xargs -n 1 basename | \
         xargs -t -n 1 -I {} cp -f `pwd`/{} ~/.dotfilez_backups/`date %+F`
     cp -r ~/.vim ~/.dotfilez_backups/`date %+F`/.vim
     cp -r ~/.xmonad ~/.dotfilez_backups/`date %+F`/.xmonad
-
 fi
 
