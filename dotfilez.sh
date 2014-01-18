d7f24dfe808a1aa9c0029b43d25248bdee42a832
zv <zv@nxvr.org>
2014-01-18 02:13:13 -0800

    Added code to link in seeeeecret keys

--- dotfilez.sh
+++ dotfilez.sh
@@ -8,6 +8,7 @@ if [ "$1" == "install" ]; then
     git ls-tree --name-only HEAD | \
         grep -v '^\.\|Makefile\|README.md\|id_rsa.gpg\|ssh_config' | \
         xargs -p -I % sh -c "ln -s $(realpath %) $HOME/.%"
+    ln -s $HOME/.Xresources $HOME/.Xdefaults
 fi
 
 if [ "$1" == "backup" ]; then
@@ -19,3 +20,24 @@ if [ "$1" == "backup" ]; then
     cp -r ~/.xmonad ~/.dotfilez_backups/`date %+F`/.xmonad
 fi
 
+# e.x ./dotfiles secrets extract || ./dotfiles secrets link
+if [ "$1" == "secrets" ]; then
+    if [ "$2" == "extract" ]; then
+        gpg --output secrets.tar.gz --decrypt keys.enc
+        tar -xvf secrets.tar.gz
+        rm secrets.tar.gz
+    fi
+
+    if [ "$2" == "link" ]; then
+        ls | grep "aws$\|ssh$\|gnupg$" | \
+        xargs -p -I % sh -c "ln -s $(realpath %) $HOME/.%"
+    fi
+
+    if [ "$2" == "repack" ]; then
+        echo 'tarring our secrets'
+        tar -czf keys.tar.gz aws gnupg ssh
+        gpg --symmetric --cipher-algo AES256 --armor -o keys.enc keys.tar.gz
+        rm keys.tar.gz
+        echo 'done'
+    fi
+fi
