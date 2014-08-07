c0a16f79d644f9031381e34b5376f73afe643459
zv <zv@nxvr.org>
2014-08-06 17:29:25 -0700

    Added some small search helper methods

--- emacs.d/personal/ag.el
+++ emacs.d/personal/ag.el
@@ -2,14 +2,16 @@
 ;;; Commentary:
 ;;; Code:
 (prelude-require-package 'ag)
+; A package for integrating ag with helm
+(prelude-require-package 'helm-ag)
 (require 'ag)
 (setq ag-highlight-search t)
-;
 
 ;;;
 ; Search
 ;;;
 (setq ag-reuse-buffers 't)
+(define-key evil-normal-state-map ",gr" 'helm-do-ag)
+(define-key evil-normal-state-map ",ga" 'ag)
 
-(define-key evil-normal-state-map ",gr" 'helm-do-grep) ; grep
-(define-key evil-normal-state-map ",ga" 'ag) ; grep
+;
