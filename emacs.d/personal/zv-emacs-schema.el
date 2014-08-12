97d7bd616dbedbc1ecec2f93dfc984843fa2331b
zv <zv@nxvr.org>
2014-08-11 17:13:16 -0700

    Remove powerline from current setup

--- emacs.d/personal/zv-emacs-schema.el
+++ emacs.d/personal/zv-emacs-schema.el
@@ -2,26 +2,9 @@
 ;;; Commentary:
 ;;;   Configure the theme, colors, etc of my Emacs setup
 ;;; Code:
-
-;;;
-; Solarized
-;;;
-(prelude-require-package 'solarized-theme)
-
 ;; make the fringe stand out from the background
-(setq solarized-distinct-fringe-background t)
-;; make the modeline high contrast
-(setq solarized-high-contrast-mode-line t)
-;; Use less bolding
-(setq solarized-use-less-bold t)
-;; Use more italics
-(setq solarized-use-more-italic t)
-;; Use less colors for indicators such as git:gutter, flycheck and similar.
-(setq solarized-emphasize-indicators nil)
-
 (setq x-underline-at-descent-line t)
-(load-theme 'solarized-dark t)
-; (load-theme 'solarized-light t)
+
 ; Default Font
 (set-frame-font "Inconsolata-11")
 ; (set-frame-font "Terminus-14")
@@ -29,14 +12,25 @@
 
 
 ;;;
-; Powerline
+; Solarized
 ;;;
-(require 'powerline)
-(setq powerline-arrow-shape 'arrow14) ;; best for small fonts
+(prelude-require-package 'solarized-theme)
+(defvar solarized-color 'dark)
+
+(defun setup-solarized-dark ()
+    (load-theme 'solarized-dark t)
+    ; (setq powerline-color1 "#073642")
+    ; (setq powerline-color2 "#002b36")
+    ;(set-face-background 'mode-line "#2aa198")
+    ;(set-face-foreground 'mode-line "#fdf6e3")
+  )
+(if (equal solarized-color 'dark)
+    (setup-solarized-dark)
+(load-theme 'solarized-light t))
+
 (setq linum-relative-current-symbol "")
-(custom-set-faces
- '(mode-line ((t (:height 90 :foreground "#030303" :background "#bdbdbd" :box nil))))
- '(mode-line-inactive ((t (:height 90 :foreground "#f9f9f9" :background "#666666" :box nil)))))
+
+
 
 ;;;
 ;; Relative Line Numbers
