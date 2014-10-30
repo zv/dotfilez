2167137c5c707888270af7253bd36996c2a9d58f
zv <zv@nxvr.org>
2014-10-29 23:32:26 -0700

    Removed prelude-based emacs.d

--- emacs.d/personal/undotree.el
+++
@@ -1,6 +0,0 @@
-; Undotree configuration
-;;; Code:
-(setq-default undo-tree-visualizer-diff t)
-
-(define-key evil-normal-state-map ",u" 'undo-tree-visualize) ; ,u for undo tree
-;
--- emacs.d/personal/yasnippet.el
+++
@@ -1,8 +0,0 @@
-;;; yasnippet.el --- Key chord setup
-;;; Commentary:
-;   Configure Yasnippet
-;;; Code:
-(prelude-require-package 'yasnippet)
-(require 'yasnippet)
-(yas-global-mode 1)
-;
