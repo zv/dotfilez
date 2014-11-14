c016435733eb4f6354f0fbe7708f7722a8459b5d
zv <zv@nxvr.org>
2014-11-13 18:35:16 -0700

    Updated contribsys

--- emacs.d/core/contribsys.el
+++ emacs.d/core/contribsys.el
@@ -1,19 +1,4 @@
 ;; Spacemacs Contribution System
-(require 'package)
-(setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")))
-(package-initialize)
-(setq warning-minimum-level :error)
-
-;; Emacs 24.3 and above ships with python.el but in some Emacs 24.3.1 packages
-;; for Ubuntu, python.el seems to be missing.
-;; This hack adds marmalade repository for this case only.
-(unless (or (package-installed-p 'python) (version< emacs-version "24.3"))
-  (add-to-list 'package-archives
-               '("marmalade" . "http://marmalade-repo.org/packages/")))
-
-(load (concat spacemacs-core-directory "ht.el"))
 
 (defconst spacemacs-dotspacemacs-version "1.0"
   "Minimum Version exepected for ~/.spacemacs file.")
@@ -27,23 +12,31 @@ keys:
 :ext-dir    the absolute path to the directory containing the extensions.
 ")
 
-(defvar spacemacs-all-packages #s(hash-table size 200 data ())
+(defvar spacemacs-all-packages #s(hash-table size 256 data ())
   "Hash table of all declared packages in all layers where the key is a package
-symbol and the value is the layer symbol where to initialize the package.")
+symbol and the value is a list of layer symbols responsible for initializing
+and configuring the package.")
 
-(defvar spacemacs-all-pre-extensions #s(hash-table size 64 data ())
+(defvar spacemacs-all-pre-extensions #s(hash-table size 128 data ())
   "Hash table of all declared pre-extensions in all layers where the key is a
-extension symbol and the value is the layer symbol where to load and
-initialize the extension.")
+extension symbol and the value is the layer symbols responsible for initializing
+and configuring the package.")
 
-(defvar spacemacs-all-post-extensions #s(hash-table size 64 data ())
+(defvar spacemacs-all-post-extensions #s(hash-table size 128 data ())
   "Hash table of all declared post-extensions in all layers where the key is a
-extension symbol and the value is the layer symbol where to load and
-initialize the extension.")
+extension symbol and the value is the layer symbols responsible for initializing
+and configuring the package.")
 
 (defvar spacemacs-contrib-layer-paths #s(hash-table size 128 data ())
   "Hash table of layers locations where the key is a layer symbol and the value
 is its path.")
+
+(defvar spacemacs-contrib-categories '()
+  "List of strings corresponding to category names. A category is a
+sub-directory of the contribution directory.")
+
+(defvar spacemacs-excluded-packages-from-layers '()
+  "List of all excluded packages declared at the layer level.")
 
 (defvar dotspacemacs-configuration-layer-path '()
   "List of additional paths where to look for configuration layers.
@@ -57,6 +50,10 @@ Paths must have a trailing slash (ie. `~/.mycontribs/')"
 (defvar dotspacemacs-fullscreen-at-startup nil
   "If non nil the frame is maximized when Emacs starts up (Emacs 24.4+ only).")
 
+(defvar dotspacemacs-feature-toggle-leader-on-jk nil
+  "If non nil pressing 'jk' in insert state, ido or helm will activate the
+evil leader.")
+
 (defvar dotspacemacs-default-package-repository 'melpa-stable
   "The default package repository used if no explicit repository has been
 specified with an installed package.
@@ -65,6 +62,23 @@ NOT USED FOR NOW :-)"
 
 (defvar dotspacemacs-excluded-packages '()
   "A list of packages and/or extensions that will not be install and loaded.")
+
+(defun contribsys/package.el-initialize ()
+  "Initialize package.el"
+  (require 'package)
+  (unless package--initialized
+    (load (concat spacemacs-core-directory "ht.el"))
+    (setq package-archives '(("ELPA" . "http://tromey.com/elpa/")
+                             ("gnu" . "http://elpa.gnu.org/packages/")
+                             ("melpa" . "http://melpa.org/packages/")))
+    (package-initialize)
+    ;; Emacs 24.3 and above ships with python.el but in some Emacs 24.3.1 packages
+    ;; for Ubuntu, python.el seems to be missing.
+    ;; This hack adds marmalade repository for this case only.
+    (unless (or (package-installed-p 'python) (version< emacs-version "24.3"))
+      (add-to-list 'package-archives
+                   '("marmalade" . "http://marmalade-repo.org/packages/")))
+    (setq warning-minimum-level :error)))
 
 (defun contribsys/dotfile-location ()
   "Return the absolute path to the spacemacs dotfile."
@@ -92,25 +106,39 @@ NOT USED FOR NOW :-)"
     (push (cons sym (list :contrib contrib :dir dir :ext-dir ext-dir))
           spacemacs-config-layers)))
 
+(defun contribsys//get-contrib-category-dirs ()
+  "Return a list of all absolute paths to the contribution categories stored
+in `spacemacs-contrib-categories'"
+  (mapcar
+   (lambda (d) (expand-file-name
+                (concat spacemacs-contrib-config-directory
+                        (format "%s/" d))))
+   spacemacs-contrib-categories))
+
 (defun contribsys/discover-contrib-layers ()
   "Fill the hash table `spacemacs-contrib-layer-paths' where the key is the
 layer symbol and the value is its path."
-  (mapc 'contribsys/discover-contrib-layers-in-dir
-        (cons spacemacs-contrib-config-directory
-              dotspacemacs-configuration-layer-path)))
+  (let ((cat-dirs (contribsys//get-contrib-category-dirs)))
+    (mapc 'contribsys/discover-contrib-layers-in-dir
+          (append (list spacemacs-contrib-config-directory)
+                  cat-dirs
+                  dotspacemacs-configuration-layer-path))))
 
 (defun contribsys/discover-contrib-layers-in-dir (dir)
   "Fill the hash table `spacemacs-contrib-layer-paths' where the key is the
-layer symbol and the value is its path for all layers found in directory DIR."
+layer symbol and the value is its path for all layers found in directory DIR.
+
+Also fill the list of excluded packages `spacemacs-excluded-packages-from-layers'
+declared at the layer level."
   (message "Looking for contribution layers in %s" dir)
   (ignore-errors
-    (let ((files (directory-files dir nil nil 'nosort)))
+    (let ((files (directory-files dir nil nil 'nosort))
+          (filter-out (append spacemacs-contrib-categories '("." ".."))))
       (dolist (f files)
-        (if (and (file-directory-p (concat dir f))
-                 (not (member f '("." ".."))))
-            (progn
-              (message "-> Discovered contribution layer: %s" f)
-              (puthash (intern f) dir spacemacs-contrib-layer-paths)))))))
+        (when (and (file-directory-p (concat dir f))
+                   (not (member f filter-out)))
+          (message "-> Discovered contribution layer: %s" f)
+          (puthash (intern f) dir spacemacs-contrib-layer-paths))))))
 
 (defun contribsys/load-layers ()
   "Load all declared layers."
@@ -133,40 +161,62 @@ layer symbol and the value is its path for all layers found in directory DIR."
           (if (file-exists-p file)
               (load file)))))))
 
+(defsubst contribsys//add-layer-to-hash (pkg layer hash)
+  "Add LAYER to the list which the value stored in HASH with key PKG."
+  (let ((list (ht-get hash pkg)))
+    (puthash pkg (add-to-list 'list layer t) hash)))
+
+(defun contribsys//add-excluded-packages (layer)
+  "Add excluded packages declared in LAYER."
+  (let ((excl-var (intern (format "%s-excluded-packages" (symbol-name layer)))))
+    (when (boundp excl-var)
+      (setq spacemacs-excluded-packages-from-layers
+            (append spacemacs-excluded-packages-from-layers
+                    (eval excl-var))))))
+
+(defsubst contribsys//filter-out-excluded-packages ()
+  "Remove excluded packages from the hash tables."
+  (mapc (lambda (h)
+          (dolist (x (ht-keys (eval h)))
+            (when (or (member x dotspacemacs-excluded-packages)
+                      (member x spacemacs-excluded-packages-from-layers))
+              (ht-remove (eval h) x))))
+        '(spacemacs-all-packages
+          spacemacs-all-pre-extensions
+          spacemacs-all-post-extensions)))
+
 (defun contribsys/read-packages-and-extensions ()
   "Load all packages and extensions declared in all layers and fill the
 corresponding hash tables:
 spacemacs-all-packages
 spacemacs-all-pre-extensions
-spacemacs-all-post-extensions
-By using a hash table we ensure that *only one layer* is responsible for the
-initialization of a package or extensions (as well as the loading in case of
-extension), the winner layer is the last layer to declare the package or
-extension.
-"
+spacemacs-all-post-extensions "
   (dolist (layer (reverse spacemacs-config-layers))
     (let* ((sym (car layer))
            (dir (plist-get (cdr layer) :dir))
            (pkg-file (concat dir "packages.el"))
            (ext-file (concat dir "extensions.el")))
-      (progn 
+      (progn
         ;; packages
-        (unless (not (file-exists-p pkg-file))
+        (when (file-exists-p pkg-file)
           (load pkg-file)
           (dolist (pkg (eval (intern (format "%s-packages" (symbol-name sym)))))
-            (unless (member pkg dotspacemacs-excluded-packages)
-              (puthash pkg sym spacemacs-all-packages))))
+            (contribsys//add-excluded-packages sym)
+            (contribsys//add-layer-to-hash pkg sym spacemacs-all-packages)))
         ;; extensions
-        (unless (not (file-exists-p ext-file))
+        (when (file-exists-p ext-file)
           (load ext-file)
           (dolist (pkg (eval (intern (format "%s-pre-extensions"
                                              (symbol-name sym)))))
-            (unless (member pkg dotspacemacs-excluded-packages)
-              (puthash pkg sym spacemacs-all-pre-extensions)))
+            (contribsys//add-excluded-packages sym)
+            (contribsys//add-layer-to-hash pkg sym
+                                           spacemacs-all-pre-extensions))
           (dolist (pkg (eval (intern (format "%s-post-extensions"
                                              (symbol-name sym)))))
-            (unless (member pkg dotspacemacs-excluded-packages)
-              (puthash pkg sym spacemacs-all-post-extensions)))))))
+            (contribsys//add-excluded-packages sym)
+            (contribsys//add-layer-to-hash pkg sym
+                                           spacemacs-all-post-extensions))))))
+  (contribsys//filter-out-excluded-packages)
   ;; number of chuncks for the loading screen
   (let ((total (+ (ht-size spacemacs-all-packages)
                   (ht-size spacemacs-all-pre-extensions)
@@ -197,12 +247,12 @@ extension.
           (dolist (pkg not-installed)
             (setq installed-count (1+ installed-count))
             (when (not (package-installed-p pkg))
-              (spacemacs/replace-last-line-of-buffer
-               (format "--> installing %s:%s... [%s/%s]"
-                       (ht-get spacemacs-all-packages pkg)
-                       pkg
-                       installed-count
-                       not-installed-count) t)
+            (spacemacs/replace-last-line-of-buffer
+             (format "--> installing %s:%s... [%s/%s]"
+                     (ht-get spacemacs-all-packages pkg)
+                     pkg
+                     installed-count
+                     not-installed-count) t)
               (package-install pkg))
             (redisplay))
           (spacemacs/append-to-buffer "\n")))))
@@ -211,29 +261,30 @@ extension.
   "Initialize all the declared packages."
   (ht-each 'contribsys/initialize-package spacemacs-all-packages))
 
-(defun contribsys/initialize-package (pkg lsym)
-  "Initialize the package PKG from the configuration layer LSYM."
-  (let* ((layer (assq lsym spacemacs-config-layers))
-         (init-func (intern (format "%s/init-%s" (symbol-name lsym) pkg))))
-    (spacemacs/loading-animation)
-    (if (and (package-installed-p pkg) (fboundp init-func))
-        (progn  (message "(Spacemacs) Initializing %s:%s..."
-                         (symbol-name lsym) pkg)
-                (funcall init-func)))))
+(defun contribsys/initialize-package (pkg layers)
+  "Initialize the package PKG from the configuration layers LAYERS."
+  (dolist (layer layers)
+    (let* ((init-func (intern (format "%s/init-%s" (symbol-name layer) pkg))))
+      (spacemacs/loading-animation)
+      (if (and (package-installed-p pkg) (fboundp init-func))
+          (progn  (message "(Spacemacs) Initializing %s:%s..."
+                           (symbol-name layer) pkg)
+                  (funcall init-func))))))
 
 (defun contribsys/initialize-extensions (ext-list)
   "Initialize all the declared extensions in EXT-LIST hash table."
   (ht-each 'contribsys/initialize-extension ext-list))
 
-(defun contribsys/initialize-extension (ext lsym)
+(defun contribsys/initialize-extension (ext layers)
   "Initialize the extension EXT from the configuration layer LSYM."
-  (let* ((layer (assq lsym spacemacs-config-layers))
-         (ext-dir (plist-get (cdr layer) :ext-dir))
-         (init-func (intern (format "%s/init-%s" (symbol-name lsym) ext))))
-       (add-to-list 'load-path (format "%s%s/" ext-dir ext))
-       (spacemacs/loading-animation)
-       (message "(Spacemacs) Initializing %s:%s..." (symbol-name lsym) ext)
-       (if (fboundp init-func) (funcall init-func))))
+  (dolist (layer layers)
+    (let* ((l (assq layer spacemacs-config-layers))
+           (ext-dir (plist-get (cdr l) :ext-dir))
+           (init-func (intern (format "%s/init-%s" (symbol-name layer) ext))))
+      (add-to-list 'load-path (format "%s%s/" ext-dir ext))
+      (spacemacs/loading-animation)
+      (message "(Spacemacs) Initializing %s:%s..." (symbol-name layer) ext)
+      (if (fboundp init-func) (funcall init-func)))))
 
 (defun contribsys/initialized-packages-count ()
   "Return the number of initialized packages and extensions."
@@ -253,7 +304,7 @@ dotspacemacs-configuration-layers defined in ~/.spacemacs."
   (let* ((layer (assq symlayer spacemacs-config-layers)))
          (plist-get (cdr layer) prop)))
 
-(defun contribsys/load-package-dependencies ()
+(defun contribsys/get-packages-dependencies ()
   "Returns a hash map where key is a dependency package symbol and value is
 a list of all packages which depend on it."
   (let ((result #s(hash-table size 200 data ())))
@@ -280,29 +331,25 @@ in `spacemacs-all-packages'"
 
 (defun contribsys/get-orphan-packages (implicit-pkgs dependencies)
   "Return a list of all orphan packages which are basically meant to be
-deleted safely. Orphan packages are packages whose all dependent packages
-are not in `spacemacs-all-packages' (explicit packages)"
+deleted safely."
   (let ((result '()))
     (dolist (imp-pkg implicit-pkgs)
-      (setq result (append result (contribsys//get-orphan-packages2
-                                   imp-pkg dependencies '()))))
-    (delete-dups result)))
+      (if (contribsys//is-package-orphan imp-pkg dependencies)
+          (add-to-list 'result imp-pkg)))
+    result))
 
-(defun contribsys//get-orphan-packages2 (pkg dependencies acc)
-  "Reccursive function to get the orphans packages as well as their
-orphan dependencies."
-  (if (and (ht-contains? dependencies pkg)
-           (not (ht-contains? spacemacs-all-packages pkg)))
-      (dolist (parent (ht-get dependencies pkg))
-        (let ((orphans (contribsys//get-orphan-packages2
-                        parent dependencies acc)))
-          (unless (not orphans)
-            (add-to-list 'acc pkg)
-            (if acc (setq acc (append acc orphans))
-              (setq acc orphans)))))
-    (unless (ht-contains? spacemacs-all-packages pkg)
-      (if acc (add-to-list 'acc pkg) (setq acc (list pkg)))))
-  acc)
+(defun contribsys//is-package-orphan (pkg dependencies)
+  "Returns not nil if PKG is an orphan package."
+  (if (ht-contains? spacemacs-all-packages pkg)
+      nil
+    (if (ht-contains? dependencies pkg)
+        (let ((parents (ht-get dependencies pkg)))
+          (reduce (lambda (x y) (and x y))
+                  (mapcar (lambda (p) (contribsys//is-package-orphan
+                                       p dependencies))
+                          parents)
+                  :initial-value t))
+      (not (ht-contains? spacemacs-all-packages pkg)))))
 
 (defun contribsys/get-package-dependencies (package)
   "Return the dependencies alist for PACKAGE."
@@ -332,11 +379,14 @@ orphan dependencies."
 (defun contribsys/delete-orphan-packages ()
   "Delete all the orphan packages."
   (interactive)
-  (let* ((dependencies (contribsys/load-package-dependencies))
+  (let* ((dependencies (contribsys/get-packages-dependencies))
          (implicit-packages (contribsys/get-implicit-packages))
          (orphans (contribsys/get-orphan-packages implicit-packages
                                                   dependencies))
          (orphans-count (length orphans)))
+    ;; (message "dependencies: %s" dependencies)
+    ;; (message "implicit: %s" implicit-packages)
+    ;; (message "orphans: %s" orphans)
     (if orphans
         (progn
           ;; for the loading dot bar
