4c54d01cd48d91988fe5873d43649ce8af8dbacd
zv <zv@nxvr.org>
2014-11-15 18:25:11 -0700

    Updated init.el to invoke new functions

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,8 +2,8 @@
 (defconst spacemacs-core-directory
   (expand-file-name (concat user-emacs-directory "core/"))
   "Spacemacs core directory.")
+(load (concat spacemacs-core-directory "contribsys.el"))
 (load (concat spacemacs-core-directory "spacemacs-mode.el"))
-
 (spacemacs/buffer)
 
 (defgroup spacemacs nil
@@ -16,21 +16,30 @@
   (defconst user-home-directory
     (expand-file-name (concat user-emacs-directory "../"))
     "User home directory (~/).")
+  (defconst spacemacs-directory
+    (expand-file-name (concat user-emacs-directory "spacemacs/"))
+    "Spacemacs base directory.")
   (defconst spacemacs-contrib-config-directory
     (expand-file-name (concat user-emacs-directory "contrib/"))
     "Spacemacs contribution layers base directory.")
+  (defconst spacemacs-cache-directory
+    (expand-file-name (concat user-emacs-directory ".cache/"))
+    "Spacemacs storage area for persistent files.")
+  (if (not (file-exists-p spacemacs-cache-directory))
+      (make-directory spacemacs-cache-directory))
   (defconst user-dropbox-directory
     (expand-file-name (concat user-home-directory "Dropbox/"))
     "Dropbox directory.")
   ;; if you have a dropbox, then ~/Dropbox/emacs is added to load path
   (add-to-list 'load-path (concat user-dropbox-directory "emacs/"))
-  (load (concat spacemacs-core-directory "contribsys.el"))
+  (contribsys/package.el-initialize)
   ;; User configuration file for Spacemacs: ~/.spacemacs
   (contribsys/load-dotfile)
   (contribsys/call-dotfile-func dotspacemacs/init)
   ;; default configuration layer of spacemacs
   (contribsys/declare-layer 'spacemacs)
   ;; configuration layers coming from `dotspacemacs-configuration-layers'
+  (setq-default spacemacs-contrib-categories '("usr" "lang"))
   (contribsys/discover-contrib-layers)
   (contribsys/declare-user-configuration-layers)
   ;; heavy lifting, load all packages and extensions
