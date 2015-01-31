17c985c8ceec25ba6923b8f43d1345601d3900fc
zv <zv@nxvr.org>
2015-01-30 21:42:09 -0800

    Skeleton of spacegnus

--- gnus.el
+++ gnus.el
@@ -1,4 +1,16 @@
-(require 'gnus)
+ ;; jeremykun
+;; nodeweekly
+;; emberweekly
+;; javascriptweekly
+;; dailyjs
+;; sitepoint
+;; ajaxian
+;; addy osmandi
+;; statuscode
+;; ericlippert 
+
+;; ember weekly http://us4.campaign-archive2.com/feed?u=ac25c8565ec37f9299ac75ca0&id=e96229d21d
+
 ;; TODO:
 ;; add key to message mode to go back to group
 ;; regularly check gmail for new messges
@@ -42,9 +54,26 @@
       '(("zv@nxvr.org" "zephyr" "mail.nxvr.org" 10025)
         ("zephyr.pellerin@gmail.com" "Zephyr Pellerin" "smtp.gmail.com" 587)))
 
-;; ---------------------------------------------------------------------------
+
+;; --------------------
+;; RSS
+;; --------------------
+
+(setq nnrss-use-local t)
+
+;; --------------------
+;; Group Parameters
+;; --------------------
+
+(setq gnus-parameters
+      '(("\\`nnrss:" 
+         (display . all)
+         (mm-discouraged-alternatives nil))))
+
+
+;; --------------------
 ;; Miscellaneous configuration parameters
-;; ---------------------------------------------------------------------------
+;; --------------------
 
 ;; Don't bother with the startup message
 (setq gnus-inhibit-startup-message t
@@ -59,7 +88,7 @@
       mm-text-html-renderer 'shr
       ;; Fetch only part of the article if we can.  I saw this in someone
       ;; else's .gnus
-      gnus-read-active-file 'some
+      ;;;; gnus-read-active-file 'some
       ;; Is the only way I found to stop gnus to
       ;; - ask me "how many articles from" and
       ;; - show-me-all-my-mail-all-ways.
@@ -92,9 +121,9 @@
         mm-file-name-replace-whitespace)
       )
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; HTML Mail Display
-;; ---------------------------------------------------------------------------
+;; --------------------
 
 (setq gnus-html-cache-directory "/tmp/gnus"
       gnus-html-frame-width 80)
@@ -104,9 +133,9 @@
 
 ;; Function to get list of links
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Window Configuration
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; • Narrow left hand side occupied by group buffer.  Right hand side
 ;;   split between summary buffer (top one-sixth) and article buffer
 ;;   (bottom).
@@ -128,7 +157,7 @@
 (gnus-add-configuration
  '(summary
    (horizontal 1.0
-               (vertical 25 (group 1.0))
+               (vertical 35 (group 1.0))
                (vertical 1.0 (summary 1.0 point)))))
 
 (if window-system
@@ -157,6 +186,16 @@
      gnus-unseen-mark      ?✩
      gnus-unread-mark      ?✉))
 
+;; --------------------
+;; Formatting and Interface
+;; --------------------
+;; add buttons
+(setq gnus-treat-buttonize t)
+(setq gnus-treat-buttonize-head 'head)
+
+;; do not display smileys as pictures
+(setq gnus-treat-display-smileys nil)
+(setq gnus-treat-fill-article 0)
 
 ;; (when (try-require 'auto-complete)
 ;;   (auto-complete-mode))
@@ -197,9 +236,9 @@
 
 (setq gnus-server-line-format "     {%(%h:%w%)} %s%a\n")
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Summary Mode
-;; ---------------------------------------------------------------------------
+;; --------------------
 (setq gnus-summary-line-format
       (concat
        "%0{%U%R%z%}"                 ;; Status
@@ -214,19 +253,62 @@
       ;; Adopt a previous line in for our false roots
       gnus-summary-make-false-root 'adopt)
 
+(setq gnus-thread-sort-functions
+      '(gnus-thread-sort-by-most-recent-date
+        gnus-thread-sort-by-number))
+
 ;; Add Keybindings ------------------------------------------
 (defun zv/gnus-summary-mode-hook  ()
   (define-key gnus-summary-mode-map "j" 'gnus-summary-next-article)
   (define-key gnus-summary-mode-map "k" 'gnus-summary-prev-article)
+
+
+  ;; previously F
+  ;; (gnus-summary-followup-with-original N &optional FORCE-NEWS)
+  (gnus-define-keys (gnus-summary-limit-map "F" gnus-summary-mode-map)
+    "/" gnus-summary-limit-to-subject
+    "n" gnus-summary-limit-to-articles
+    "b" gnus-summary-limit-to-bodies
+    "h" gnus-summary-limit-to-headers
+    "w" gnus-summary-pop-limit
+    "s" gnus-summary-limit-to-subject
+    "a" gnus-summary-limit-to-author
+    "u" gnus-summary-limit-to-unread
+    "m" gnus-summary-limit-to-marks
+    "M" gnus-summary-limit-exclude-marks
+    "v" gnus-summary-limit-to-score
+    "*" gnus-summary-limit-include-cached
+    "D" gnus-summary-limit-include-dormant
+    "T" gnus-summary-limit-include-thread
+    "d" gnus-summary-limit-exclude-dormant
+    "t" gnus-summary-limit-to-age
+    "." gnus-summary-limit-to-unseen
+    "x" gnus-summary-limit-to-extra
+    "p" gnus-summary-limit-to-display-predicate
+    "E" gnus-summary-limit-include-expunged
+    "c" gnus-summary-limit-exclude-childless-dormant
+    "C" gnus-summary-limit-mark-excluded-as-read
+    "o" gnus-summary-insert-old-articles
+    "N" gnus-summary-insert-new-articles
+    "S" gnus-summary-limit-to-singletons
+    "r" gnus-summary-limit-to-replied
+    "R" gnus-summary-limit-to-recipient
+    "A" gnus-summary-limit-to-address)
+
+
+  
+  (mapc (lambda (gk) (guide-key/add-local-guide-key-sequence gk))
+        '("M" "/" "Y" "S" "V" "A" "X" "B" "O" "W" "F"))
+
   ;; Ensure our global bindings are not overridden
   (define-key gnus-summary-mode-map prev-buffer-key 'evil-window-prev)
   (define-key gnus-summary-mode-map next-buffer-key 'evil-window-next))
 
 (add-hook 'gnus-summary-mode-hook 'zv/gnus-summary-mode-hook)
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Group Mode
-;; ---------------------------------------------------------------------------
+;; --------------------
 (setq gnus-group-line-format "%P%5y:%B%(%uG%) %M%S%p \n")
 
 (defun gnus-user-format-function-G (_arg)
@@ -235,50 +317,130 @@
 ;; • Keep track of the last time we opened this group.
 (add-hook 'gnus-select-group-hook 'gnus-group-set-timestamp)
 
-(add-hook 'org-mode-hook 'guide-key/my-hook-function-for-org-mode)
-
-
-(add-hook 'gnus-group-mode-hook 'gnus-topic-mode)
-
 (defun zv/gnus-group-mode-hook ()
-  ;; • Define our keys
-  (zv/define-keymap 'gnus-group-mode-map zv-gnus-group-keymap)
+  (evil-set-initial-state 'gnus-group-mode 'motion)
 
+  ;; • Define our keys
+  (evil-define-key 'motion gnus-group-mode-map
+    ;;"/"  'evil-search-forward
+    ;; "?"  'evil-search-backward
+    "r"  'gnus-topic-get-new-news-this-topic
+    (kbd "RET") ''gnus-topic-select-group
+    (kbd "M-RET") ''gnus-group-visible-select-group
+    "=" 'gnus-group-select-group
+    "\r" 'gnus-group-select-group
+    "\M-\r" 'gnus-group-quick-select-group
+    "\M- " 'gnus-group-visible-select-group
+    "f" 'gnus-group-jump-to-group
+    "J" 'gnus-group-next-unread-group
+    "K" 'gnus-group-prev-unread-group
+    "j" 'gnus-group-next-group
+    "k" 'gnus-group-prev-group
+    "[" 'gnus-topic-goto-previous-topic
+    "]" 'gnus-topic-goto-next-topic
+    "\M-j" 'gnus-group-next-unread-group-same-level
+    "\M-k" 'gnus-group-prev-unread-group-same-level
+    ;;"," 'gnus-group-best-unread-group
+    ;;"." 'gnus-group-first-unread-group
+    "u" 'gnus-group-unsubscribe-current-group
+    "c" 'gnus-group-catchup-current
+    "C" 'gnus-group-catchup-current-all
+    "\M-c" 'gnus-group-clear-data
+    "l" 'gnus-group-list-groups
+    "L" 'gnus-group-list-all-groups
+    "m" 'gnus-group-mail
+    "i" 'gnus-group-news
+    "g" 'gnus-group-get-new-news
+    "\M-g" 'gnus-group-get-new-news-this-group
+    "R" 'gnus-group-restart
+    "B" 'gnus-group-browse-foreign-server
+    "b" 'gnus-group-check-bogus-groups
+    "a" 'gnus-group-post-news
+    ;; visual 
+    ;; "x" 'gnus-group-kill-region
+    "\C-c\C-x" 'gnus-group-expire-articles
+    "\C-c\M-\C-x" 'gnus-group-expire-all-groups
+    "V" 'gnus-version
+    "q" 'gnus-group-exit
+    "Q" 'gnus-group-quit
+    "^" 'gnus-group-enter-server-mode
+    "s" 'gnus-group-sort-groups
+    "t" 'gnus-topic-mode
+    "\M-&" 'gnus-group-universal-argument
+    "o" 'gnus-group-mark-group
+    "\M-o" 'gnus-group-unmark-group
+    
+    "Du" 'gnus-sieve-update
+    "Dg" 'gnus-sieve-generate
+
+    "Sl" 'gnus-group-set-current-level
+    "St" 'gnus-group-unsubscribe-current-group
+    "Ss" 'gnus-group-unsubscribe-group
+    "Sk" 'gnus-group-kill-group
+    "Sy" 'gnus-group-yank-group
+    "Sw" 'gnus-group-kill-region
+    "S\C-k" 'gnus-group-kill-level
+    "Sz" 'gnus-group-kill-all-zombies)
+
+  (evil-leader/set-key-for-mode 'gnus-group-mode
+    "msn" 'gnus-group-save-newsrc
+    ;; Other
+    "mkl" 'gnus-group-edit-local-kill
+    "mkl" 'gnus-group-edit-global-kill
+    "mgk" 'gnus-group-kill-group
+    "mgy" 'gnus-group-yank-group
+    ;; [w]ho?
+    "mgW" 'gnus-group-list-killed
+    ;; Group manipulation
+    "mgm" 'gnus-group-make-group
+    "mgE" 'gnus-group-edit-group
+    "mge" 'gnus-group-edit-group-method
+    "mgp" 'gnus-group-edit-group-parameters
+    "mgv" 'gnus-group-add-to-virtual
+    "mgD" 'gnus-group-enter-directory
+    "mgr" 'gnus-group-rename-group
+    "mgc" 'gnus-group-customize
+    "mgz" 'gnus-group-compact-group
+    "mgx" 'gnus-group-expunge-group
+    "mgd" 'gnus-group-make-directory-group
+    "mgh" 'gnus-group-make-help-group
+    "mgu" 'gnus-group-make-useful-group
+    "mgl" 'gnus-group-nnimap-edit-acl
+    "mgV" 'gnus-group-make-empty-virtual
+    "mgf" 'gnus-group-make-doc-group
+    "mgw" 'gnus-group-make-web-group
+    "mgg" 'gnus-group-make-nnir-group
+    "mgR" 'gnus-group-make-rss-group
+    "mgM" 'gnus-group-read-ephemeral-group
+    "mg<backspace>" 'gnus-group-delete-group
+    ;; Sort
+    "mss" 'gnus-group-sort-groups
+    "msa" 'gnus-group-sort-groups-by-alphabet
+    "msu" 'gnus-group-sort-groups-by-unread
+    "msl" 'gnus-group-sort-groups-by-level
+    "msv" 'gnus-group-sort-groups-by-score
+    "msr" 'gnus-group-sort-groups-by-rank
+    "msm" 'gnus-group-sort-groups-by-method
+    "msn" 'gnus-group-sort-groups-by-real-name)
+ 
   (guide-key/add-local-guide-key-sequence "G")
   (guide-key/add-local-guide-key-sequence "T")
+  (guide-key/add-local-guide-key-sequence "S")
+  (guide-key/add-local-guide-key-sequence "J")
 
   (define-key gnus-group-mode-map "z" nil)
   (define-key gnus-group-mode-map prev-buffer-key 'evil-window-prev)
   (define-key gnus-group-mode-map next-buffer-key 'evil-window-next)
 
   ;; • Ensure our group mode automatically adjusts to `Topic'
-  (add-hook 'gnus-group-mode-hook 'gnus-topic-mode))
+  (gnus-topic-mode))
 
 (add-hook 'gnus-group-mode-hook 'zv/gnus-group-mode-hook)
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Article Mode
-;; ---------------------------------------------------------------------------
+;; --------------------
 (require 'gnus-art)
-
-(add-hook 'gnus-article-mode-hook
-          (lambda ()
-            (evil-leader/set-key-for-mode 'gnus-article-mode
-              "maf" 'mml-insert-part
-              ;; Attach directory
-              "mad" 'gnus-dired-attach
-              "mep" 'mml-secure-message-encrypt-pgp
-              "mer" 'mml-unsecure-message)))
-
-(evil-leader/set-key-for-mode 'gnus-group-mode "ms" 'server)
-
-;; Prefixes
-;; t - Topics
-
-;; formatting and interface -----------------------------------------------------
-(setq gnus-thread-sort-functions
-      '((not gnus-thread-sort-by-date)
-        (not gnus-thread-sort-by-number)))
 
 (setq gnus-visible-headers
       "^From:\\|^To:\\|^Subject:\\|^Date:\\|^User-Agent:\\|^X-Mailer:"
@@ -286,30 +448,61 @@
       gnus-topic-indent-level 1
       gnus-group-uncollapsed-levels 2)
 
-
-
-;; add buttons
-(setq gnus-treat-buttonize t)
-(setq gnus-treat-buttonize-head 'head)
-
-;; do not display smileys as pictures
-(setq gnus-treat-display-smileys nil)
-(setq gnus-treat-fill-article 0)
-
 ;; format specification for the article mode line
 (setq gnus-article-mode-line-format "%S%m")
 
-;; make `C-c C-f' active from within messages
-;;(define-key gnus-article-mode-map
-;;  (kbd "C-c C-f") 'gnus-summary-mail-forward)
+(defun zv/gnus-article-mode-hook ()
+  (zv/define-keymap gnus-article-mode-map
+                    '(("n" . shr-next-link)
+                      ("p" . shr-previous-link)))
+  
+    )
 
-;; ---------------------------------------------------------------------------
+(add-hook 'gnus-article-mode-hook 'zv/gnus-article-mode-hook)
+
+;; Article Edit Mode
+(evil-set-initial-state 'gnus-article-edit-mode-map 'normal)
+(evil-leader/set-key-for-mode gnus-article-edit-mode-map
+  "maf" 'mml-attach-file
+  "ms" 'gnus-article-edit-done
+  "mQ" 'gnus-article-edit-done)
+
+(evil-define-key 'normal gnus-article-edit-mode-map
+  "gt"    'message-goto-to
+  "go"    'message-goto-from
+  "gb"    'message-goto-bcc
+  "gc"    'message-goto-cc
+  "gs"    'message-goto-subject
+  "gr"    'message-goto-reply-to
+  "gn"    'message-goto-newsgroups
+  "gf"    'message-goto-followup-to
+  "gm"    'message-goto-mail-followup-to
+  "gu"    'message-goto-summary
+  "gi"    'message-insert-or-toggle-importance
+  "ga"    'message-generate-unsubscribed-mail-followup-to
+
+  "gb"    'message-goto-body
+  "gi"    'message-goto-signature
+
+  "Mt"    'message-insert-to
+  "Mn"    'message-insert-newsgroups
+
+  "So"    'message-sort-headers
+  "de"    'message-elide-region
+  "dz"    'message-kill-to-signature
+
+  "\M-\r" 'message-newline-and-reformat
+  "\t"    'message-tab)
+
+;; --------------------
 ;; Message Mode
-;; ---------------------------------------------------------------------------
+;; --------------------
+
 ;; Server Browse Mode
 (evil-set-initial-state 'message-mode 'normal)
 
 (defun zv/message-mode-hook ()
+  (evil-set-initial-state 'message-mode 'normal)
   ;; Initialize BBDB
   ;; (bbdb-initialize 'message)
   ;; (bbdb-initialize 'gnus)
@@ -318,10 +511,6 @@
   ;; Setup formatting
   (setq fill-column 72)
   (turn-on-auto-fill)
-
-  ;; Turn on message mode bindings
-  (evil-leader/set-key-for-mode 'gnus-message-mode
-    "mb" 'bbdb)
 
   ;; tab completion for alias in `.mailrc'
   (local-set-key (kbd "<M-tab>") 'mail-abbrev-complete-alias)
@@ -348,11 +537,65 @@
     ;; no tag marking the beginning of footnote section
     (set (make-local-variable
           'org-footnote-tag-for-non-org-mode-files) nil))
+
+  (evil-define-key 'normal message-mode-map
+    "gt" 'message-goto-to
+    "go" 'message-goto-from
+    "gb" 'message-goto-body
+    "gw" 'message-goto-fcc
+    "gc" 'message-goto-cc
+    "gz" 'message-goto-signature
+    "gs" 'message-goto-subject
+    "gr" 'message-goto-reply-to
+    "gn" 'message-goto-newsgroups
+    "gd" 'message-goto-distribution
+    "gf" 'message-goto-followup-to
+    "gm" 'message-goto-mail-followup-to
+    "gk" 'message-goto-keywords
+    "gu" 'message-goto-summary
+    "gi" 'message-insert-or-toggle-importance
+    "de" 'message-kill-address
+    "dr" 'message-elide-region
+    "dz" 'message-kill-to-signature
+    "\M-\r" 'message-newline-and-reformat
+    "\M-n" 'message-display-abbrev
+    "\C-c\C-f\C-a" 'message-generate-unsubscribed-mail-followup-to)
+
+  (evil-leader/set-key-for-mode 'message-mode
+    ;; [m]essage [d]eclare
+    "mdr" 'message-mark-inserted-region
+    "mdf" 'message-mark-insert-file
+    ;; [m]essage [i]nsert
+    "mit" 'message-insert-to
+    "miw" 'message-insert-wide-reply
+    "min" 'message-insert-newsgroups
+    "mil" 'message-to-list-only
+    "mix" 'message-insert-expires
+    "mis" 'message-insert-signature
+    "mih" 'message-insert-headers
+    "miu" 'message-insert-or-toggle-importance
+    "mid" 'message-insert-disposition-notification-to
+    ;; [m]essage [f]uck
+    "mfx" 'message-cross-post-followup-to
+    "mft" 'message-reduce-to-to-cc
+    "mfa" 'message-add-archive-header
+    ;; [m]essage [y]ank
+    "myy" 'message-yank-original
+    "myb" 'message-yank-buffer
+    "mms" 'message-sort-headers
+
+    ;; Direct message items
+    "ms" 'message-send-and-exit
+    "mq" 'message-kill-buffer
+    "mmr" 'message-rename-buffer
+    "mms" 'message-send
+    "mmd" 'gnus-delay-article
+    "mmb" 'message-dont-send)
   )
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Server Mode
-;; ---------------------------------------------------------------------------
+;; --------------------
 
 ;; Server Browse Mode
 (evil-set-initial-state 'gnus-browse-mode 'motion)
@@ -366,9 +609,9 @@
                        ("q" . gnus-browse-exit))))
 
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Caching
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; use the cache
 (setq gnus-use-cache t)
 
@@ -387,9 +630,9 @@
 ;; avoid caching your nnml and nnfolder groups
 (setq gnus-uncacheable-groups "^nnml\\|^nnfolder")
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Message Encryption
-;; ---------------------------------------------------------------------------
+;; --------------------
 (use-package epg-config
   :init
   ;; Sign our messages with an attachment
@@ -449,9 +692,9 @@
    (define-key gnus-summary-mode-map (kbd "C-c k") 'gnus-article-receive-epg-keys)))
 
 
-;; ---------------------------------------------------------------------------
+;; --------------------
 ;; Forwarding
-;; ---------------------------------------------------------------------------
+;; --------------------
 
 ;; delimiter inserted before forwarded messages
 (setq message-forward-start-separator "-----Original Message-----\n")
