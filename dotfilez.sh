e65c6dc317616c213d23a56861d2d3e12c2e3f27
zv <zv@nxvr.org>
2015-03-03 15:31:33 -0700

    Bunch of changes to dotfilez.sh

--- dotfilez.sh
+++ dotfilez.sh
@@ -1,14 +1,8 @@
 #!/bin/zsh
 
-# install dotfiles one by one
-# I make absolutely zero promises on how this will work with OSX coreutils
-# or any other OS toolchain currently experiencing a time warp to late '80s
-
-
-
 function build_emacs {
     # Dependencies
-    sudo yum -y install atk-devel cairo-devel freetype-devel \
+    sudo dnf -y install atk-devel cairo-devel freetype-devel \
          fontconfig-devel dbus-devel giflib-devel glibc-devel gtk2-devel \
          libpng-devel libjpeg-devel libtiff-devel libX11-devel libXau-devel \
          libXdmcp-devel libXrender-devel libXt-devel libXpm-devel \
@@ -22,23 +16,23 @@ function build_emacs {
         ./configure --prefix=/usr/local/emacs24 --with-x-toolkit=gtk --with-gnutls --with-file-notification=yes && \
         make bootstrap && \
         make -j4 &&
-        (sudo alternatives --install /usr/bin/emacs emacs /usr/local/emacs24/bin/emacs 20000; 
+        (sudo alternatives --install /usr/bin/emacs emacs /usr/local/emacs24/bin/emacs 20000;
          sudo alternatives --install /usr/bin/emacsclient emacsclient /usr/local/emacs24/bin/emacsclient 20000)
     ## Build org mode
-    cd ~/emacs.d/contrib/zv/org-mode
-    make && sudo make install-info
-}
-
-function encrypt_setup {
-    gpg2 -a -r "zv@nxvr.org" --encrypt-files id_rsa config known_hosts *.pem
+    # cd ~/emacs.d/contrib/zv/org-mode
+    # make && sudo make install-info
+    # rust && cargo && multirust
 }
 
 case $1 in
+    # install dotfiles one by one
+    # I make absolutely zero promises on how this will work with OSX coreutils
+    # or any other OS toolchain currently experiencing a time warp to late '80s
     install)
         # List of files & directories not to link
         typeset -aU nolink
-        nolink=("^\." Makefile dotfilez.sh org lib ida.cfg ssh_config
-        ebin zv.gpg.pub README.md id_rsa.gpg)
+        nolink=("^\." certificates Makefile org lib ida.cfg ssh_config
+                ebin zv.gpg.pub README.md .gpg .sh)
         # Now link our files
         git ls-tree --name-only HEAD | grep -v "${(j:\|:)nolink}" |\
             xargs -p -I % sh -c "ln -s $(realpath %) $HOME/.%"
@@ -51,40 +45,69 @@ case $1 in
         # Link Xresources to Xdefaults
         ln -s $HOME/.Xresources $HOME/.Xdefaults
         ;;
+    backup_secrets)
+        gpg --export-ownertrust > zv-ownertrust-gpg.txt
+        # Tar our setup
+        tar -cf backup.tar ssh/{id_rsa,id_ecdsa,other_keys.seahorse} gnupg/pubring.gpg gnupg/secring.gpg zv-ownertrust-gpg.txt
+        gpg --output backup.tar.gpg --symmetric backup.tar
+        rm -f zv-ownertrust-gpg.txt
+        rm -f backup.tar
+        ;;
+    extract_secrets)
+        gpg --decrypt backup.tar.gpg > backup.tar # && tar -xvf backup.tar && rm -f backup.tar
+        ;;
 esac
+
+function print_zv_greetz {
+    print -P "z  %F{green}███████%F{red}╗%F{green}██%F{red}╗   %F{green}██%F{red}╗%f  z"
+    print -P "e  %F{red}╚══%F{green}███%F{red}╔╝%F{green}%F{green}██%F{red}║   %F{green}██%f%F{red}║%f  e"
+    print -P "t    %F{green}███%F{red}╔╝ %F{green}██%F{red}║   %F{green}██%F{red}║%f  t"
+    print -P "a   %F{green}███%F{red}╔╝  %F{red}╚%f%F{green}██%F{red}╗ %F{green}██%f%F{red}╔╝%f  a"
+    print -P "v  %F{green}███████%F{red}╗ %F{red}╚%f%F{green}████%f%F{red}╔╝%f   v"
+    print -P "o  %F{red}╚══════╝  %F{red}╚═══╝%f    o"
+    print -P "l  too hot to handle  l"
+    print -P "t  too cold to hold.  t"
+}
+
+###
+# This function downloads and installs all the fonts I use.
+###
+function get_fonts {
+    wget https://github.com/adobe-fonts/source-code-pro/releases/tag/1.017R
+
+
+    print -P "%F{green}Installing Powerline Fonts...%f"
+    (git clone git@github.com:powerline/fonts.git /tmp && /tmp/install.sh)&
+}
 
 # What needs to get done for decrypting
 # decrypt all ssh/*.asc files
 # decrypt my org files
 # What needs to get done for package install
 # build emacs from source
-
-print -P "z  %F{green}███████%F{red}╗%F{green}██%F{red}╗   %F{green}██%F{red}╗%f  z"
-print -P "e  %F{red}╚══%F{green}███%F{red}╔╝%F{green}%F{green}██%F{red}║   %F{green}██%f%F{red}║%f  e"
-print -P "t    %F{green}███%F{red}╔╝ %F{green}██%F{red}║   %F{green}██%F{red}║%f  t"
-print -P "a   %F{green}███%F{red}╔╝  %F{red}╚%f%F{green}██%F{red}╗ %F{green}██%f%F{red}╔╝%f  a"
-print -P "v  %F{green}███████%F{red}╗ %F{red}╚%f%F{green}████%f%F{red}╔╝%f   v"
-print -P "o  %F{red}╚══════╝  %F{red}╚═══╝%f    o"
-print -P "l  too hot to handle  l"
-print -P "t  too cold to hold.  t"
-
-function setup_env() {
+function setup_env {
+    dnf check-update
     # Setting up environment
     # wow how fucked up is this
     # I have to put this in an eval sublist
-    # in order to not escape 
-    mkdir ~/{Development, Books, bin}
-    ln -s $HOME/z ~/Development
-    ln -s $HOME/Z ~/Development
+    # in order to not escape
+    print "%F{cyan}Making Personal Directories%f"
+    mkdir ~/{Development,Books,bin}
 
-    print -P "%F{green}Installing local packages%f"
-    yum -y install yum-utils
+    print "%F{cyan}Linking Personal Directories%f"
+    ln -s $HOME/Development/ $HOME/z
+    ln -s $HOME/Development/ $HOME/z
+
+    print -P "%F{green}Installing local packages, grab some coffee%f"
+
     # Development tools
     ## List of yum groups to be installed
     groups=(
         'C Development Tools and Libraries'
         'Development Tools'
+        'System Tools'
     )
+
     # List of packages to be installed
     packages=(
         ack
@@ -115,7 +138,6 @@ function setup_env() {
         scrot
         socat
         the_silver_searcher
-        levien-inconsolata-fonts
         tmux
         vim-enhanced
         weechat
@@ -124,18 +146,19 @@ function setup_env() {
         xmonad
         zsh
     )
-    yum -y groupinstall "${(j: :)groups}"
-    yum install -y "${(j: :)packages}"
 
-    print -P "%F{green}Installing NVM...%f"
+    dnf group install -y "${(j: :)groups}"
+    dnf install -y "${(j: :)packages}"
+
     ## Install NVM
-    curl https://raw.githubusercontent.com/creationix/nvm/v0.23.2/install.sh | bash
+    #print -P "%F{green}Installing NVM...%f"
+    #(curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash)&
 
-    print -P "%F{green}Installing Lein...%f"
+    #print -P "%F{green}Installing Lein...%f"
+    #(curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin )&
 
-    curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin
+    # print -P "%F{green}Building Emacs...%f"
+    # build_emacs
 
-    build_emacs
-
-    systemctl unmask tmp.mount
+    # systemctl unmask tmp.mount
 }
