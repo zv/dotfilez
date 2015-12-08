a48f80bcf53c0541e02e20454ef245de7e48f4d1
Zephyr Pellerin <zv@nxvr.org>
2015-12-08 00:48:36 -0800

    [dotfilez.sh] Added encrypt/decrypt routines

--- dotfilez.sh
+++ dotfilez.sh
@@ -1,102 +1,44 @@
 #!/bin/zsh
+# My personal key
+typeset key_id='0xF6F2D0445DC172F8'
+# Set to `true` to install yubikey dependencies
+typeset twofa_dependencies="false"
+typeset -a protected groups packages npm_packages
 
-function build_emacs {
-    # Dependencies
-    sudo dnf -y install atk-devel cairo-devel freetype-devel \
-         fontconfig-devel dbus-devel giflib-devel glibc-devel gtk2-devel \
-         libpng-devel libjpeg-devel libtiff-devel libX11-devel libXau-devel \
-         libXdmcp-devel libXrender-devel libXt-devel libXpm-devel \
-         ncurses-devel xorg-x11-proto-devel zlib-devel librsvg2-devel \
-         m17n-lib-devel libotf-devel autoconf automake bzip2 cairo texinfo \
-         gzip GConf2-devel alsa-lib-devel desktop-file-utils python2-devel \
-         python3-devel util-linux gnutls-devel && \
-        ## Build emacs
-        git clone -b emacs-24.4 git://git.sv.gnu.org/emacs.git ~/Development/emacs && \
-        ./autogen && \
-        ./configure --prefix=/usr/local/emacs24 --with-x-toolkit=gtk --with-gnutls --with-file-notification=yes && \
-        make bootstrap && \
-        make -j4 &&
-        (sudo alternatives --install /usr/bin/emacs emacs /usr/local/emacs24/bin/emacs 20000;
-         sudo alternatives --install /usr/bin/emacsclient emacsclient /usr/local/emacs24/bin/emacsclient 20000)
-    ## Build org mode
-    # cd ~/emacs.d/contrib/zv/org-mode
-    # make && sudo make install-info
-    # rust && cargo && multirust
-}
+###
+# Pack/Unpack my encrypted documents and configuration examples
+###
+local -aU protected
+protected=(
+    irssi/sasl.auth
+    ssh/id_{rsa,ed25519}
+    ssh/config
+    org/*
+    certificates/{nxvr.crt,gpgsm-nxvr.crt,gpsm-nxvr.csr,startssl-nxvr.crt,README}
+)
 
-case $1 in
-    # install dotfiles one by one
-    # I make absolutely zero promises on how this will work with OSX coreutils
-    # or any other OS toolchain currently experiencing a time warp to late '80s
-    install)
-        # List of files & directories not to link
-        typeset -aU nolink
-        nolink=("^\." certificates Makefile org lib ida.cfg ssh_config
-                ebin zv.gpg.pub README.md .gpg .sh)
-        # Now link our files
-        git ls-tree --name-only HEAD | grep -v "${(j:\|:)nolink}" |\
-            xargs -p -I % sh -c "ln -s $(realpath %) $HOME/.%"
-
-        # compile erlang our prompt here...
-        if [[ -x erlc ]]; then
-            erlc -o ebin ebin/zv_shell.erl
-        fi
-
-        # Link Xresources to Xdefaults
-        ln -s $HOME/.Xresources $HOME/.Xdefaults
-        ;;
-    backup_secrets)
-        gpg --export-ownertrust > zv-ownertrust-gpg.txt
-        # Tar our setup
-        tar -cf backup.tar ssh/{id_rsa,id_ecdsa,other_keys.seahorse} gnupg/pubring.gpg gnupg/secring.gpg zv-ownertrust-gpg.txt
-        gpg --output backup.tar.gpg --symmetric backup.tar
-        rm -f zv-ownertrust-gpg.txt
-        rm -f backup.tar
-        ;;
-    extract_secrets)
-        gpg --decrypt backup.tar.gpg > backup.tar # && tar -xvf backup.tar && rm -f backup.tar
-        ;;
-esac
-
-function print_zv_greetz {
-    print -P "z  %F{green}███████%F{red}╗%F{green}██%F{red}╗   %F{green}██%F{red}╗%f  z"
-    print -P "e  %F{red}╚══%F{green}███%F{red}╔╝%F{green}%F{green}██%F{red}║   %F{green}██%f%F{red}║%f  e"
-    print -P "t    %F{green}███%F{red}╔╝ %F{green}██%F{red}║   %F{green}██%F{red}║%f  t"
-    print -P "a   %F{green}███%F{red}╔╝  %F{red}╚%f%F{green}██%F{red}╗ %F{green}██%f%F{red}╔╝%f  a"
-    print -P "v  %F{green}███████%F{red}╗ %F{red}╚%f%F{green}████%f%F{red}╔╝%f   v"
-    print -P "o  %F{red}╚══════╝  %F{red}╚═══╝%f    o"
-    print -P "l  too hot to handle  l"
-    print -P "t  too cold to hold.  t"
-}
+function protect   { gpg -a -r "$key_id" --encrypt-files ${(@)protected} }
+function unprotect { gpg --decrypt-files **/*.asc }
 
 ###
 # This function downloads and installs all the fonts I use.
 ###
-function get_fonts {
-    wget https://github.com/adobe-fonts/source-code-pro/releases/tag/1.017R
-
-
+function install_fonts {
+    local tmp_font_dir
+    pushd
+    tmp_font_dir=mktemp -d
+    cd "$tmp_font_dir"
+    print -P "%F{green}Installing Source Code Pro Fonts...%f"
+    sudo dnf install adobe-source-code-pro-fonts
     print -P "%F{green}Installing Powerline Fonts...%f"
-    (git clone git@github.com:powerline/fonts.git /tmp && /tmp/install.sh)&
+    (git clone git@github.com:powerline/fonts.git && install.sh)&
+    popd
 }
 
-# What needs to get done for decrypting
-# decrypt all ssh/*.asc files
-# decrypt my org files
-# What needs to get done for package install
-# build emacs from source
-function setup_env {
+function install_pkgs {
     dnf check-update
-    # Setting up environment
-    # wow how fucked up is this
-    # I have to put this in an eval sublist
-    # in order to not escape
-    print "%F{cyan}Making Personal Directories%f"
-    mkdir ~/{Development,Books,bin}
 
-    print "%F{cyan}Linking Personal Directories%f"
-    ln -s $HOME/Development/ $HOME/z
-    ln -s $HOME/Development/ $HOME/z
+    dnf update
 
     print -P "%F{green}Installing local packages, grab some coffee%f"
 
@@ -115,32 +57,57 @@ function setup_env {
         clang
         clang-devel
         clang-libs
+        curl
         dnsenum
         dnsmap
         emacs
-        curl
+        # git-extras
+        global # gtags
+        gnupg2
+        graphviz
+        hping3
+        i3
+        i3lock
+        i3status
         libvirt
         mtr
-        git-extras
-        global # gtags
         nmap
-        hping3
         rlwrap
         rpmdevtools
         scapy
-        gnupg2
-        graphviz
         socat
         the_silver_searcher
+        thunderbird
         tmux
         vim-enhanced
+        vlc
         wireshark
         xbacklight
         zsh
     )
 
-    dnf group install -y "${(j: :)groups}"
-    dnf install -y "${(j: :)packages}"
+    if [[ "$twofa_dependencies" == 'true' ]]; then
+      sudo dnf install ykpers-devel libyubikey-devel libusb-devel autoconf gnupg gnupg2-smime pcsc-lite
+    fi
+
+    dnf group install -y ${(@)groups}
+    dnf install -y ${(@)packages}
+}
+
+
+function configure_env {
+    # Set my shell to zsh
+    chsh -s =zsh zv
+
+    # Setting up environment
+    # wow how fucked up is this
+    # I have to put this in an eval sublist
+    # in order to not escape
+    print "%F{cyan}Making Personal Directories%f"
+    mkdir ~/{Development,Books,bin}
+
+    print "%F{cyan}Linking Personal Directories%f"
+    ln -s $HOME/Development/ $HOME/z
 
     ## Install NVM
     #print -P "%F{green}Installing NVM...%f"
@@ -149,29 +116,55 @@ function setup_env {
     #print -P "%F{green}Installing Lein...%f"
     #(curl https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > ~/bin )&
 
-    # print -P "%F{green}Building Emacs...%f"
-    # build_emacs
+    # Install my custom /etc files
+    for f in etc/**/*; do
+        cp -i "/$f" "$f"
+    done
 
-    # systemctl unmask tmp.mount
+    # Link a few user systemd unit files
+    for uf in systemd/user/default.target.wants/*; do
+        systemctl --user link "$uf"
+    done
+
+    # Import certificates into gpg
+    gpg --import certificates/*
 }
 
-#i3bar i3lock
+function javascript {
+    ## Build nodejs
+    pushd
+    mkdir -p ~/Development/node/local
+    cd ~/Development/node
+    curl https://nodejs.org/dist/node-latest.tar.gz | tar xz --strip-components=1
+    ./configure --prefix=~/Development/node/local
+    make install
+    curl https://www.npmjs.org/install.sh | sh
+    rehash npm
 
-# emacs js mode
-# $ npm install -g js-beautify
-# thunderbird && thunderbird-enigmail
-# sudo dnf install ykpers-devel libyubikey-devel libusb-devel autoconf gnupg gnupg2-smime pcsc-lite
-# install zeal git@github.com:zealdocs/zeal.git # salimma/zeal
-# trackpad thing
-# xinput --set-prop 14 140 1.5 0 0 0 1.5 0 0 0 1
+    ## Install our NPM packages
+    npm_packages=(ternjs js-beautify yo ember-cli bower babel-core babel-cli)
 
-# This will list the properties of your mouse. You'll see your Coordinate Transformation Matrix there, remember the number between parenthesis, mine is 146.
-# .
+    npm install -g ${(@)npm_packages}
+    popd
+}
 
-# $xinput --set-prop 8 146 2 0 0 0 2 0 0 0 1
-
-# The numbers to pay attention here are the first and second '2' digits. The first specifies your x-axis sensitivity, and the second your y-axis sensitivity. Those numbers are generally set at 1, if you increase them you get faster mouse movement, you can also specify floating point numbers to decrease the speed(0.5 will half the speed). You can also change the last digit, which is the scale, it will multiply itself by both the x-axis and y-axis sensitivity, so you can change the mouse sensitivity with a single change.
-
-# One thing to remember though, these changes to the Coordinate Transformation Matrix don't play well with steam games, I have no idea why. Any change to it will mess up the mouse movement in-game, in my case the mouse just trembles in the top left corner of the screen, the game is unplayable. To fix it you need to return the matrix to 1's.
-
-# $xinput --set-prop 8 146 1 0 0 0 1 0 0 0 1
+case $1 in
+    # install dotfiles one by one
+    # I make absolutely zero promises on how this will work with OSX coreutils
+    # or any other OS toolchain currently experiencing a time warp to late '80s
+    install)
+        # List of files & directories not to link
+        typeset -aU nolink
+        nolink=("^\." certificates Makefile org lib ida.cfg ssh_config
+                ebin zv.gpg.pub README.md .gpg .sh)
+        # Now link our files
+        git ls-tree --name-only HEAD | grep -v "${(j:\|:)nolink}" |\
+            xargs -p -I % sh -c "ln -s $(realpath %) $HOME/.%"
+        ;;
+    protect)
+        protect
+        ;;
+    unprotect)
+        unprotect
+        ;;
+esac
