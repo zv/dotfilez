8f5da5dd86215081c6a7493ca59577c0371994b6
Zephyr Pellerin <zv@nxvr.org>
2016-01-10 19:25:07 -0800

    [thunderbird] Updated backup.sh to include feed material

--- thunderbird/backup.sh
+++ thunderbird/backup.sh
@@ -1,18 +1,27 @@
 #!/usr/bin/env zsh
 function backup_profile {
-  local profile_path=~/.thunderbird/zv.default
-  if [[ -e $profile_path ]]; then
-      echo "Creating profile..."
-      tar --create \
-          --exclude-from=thunderbird_exclude \
-          -C ~/.thunderbird \
-          -aJ zv.default \
-          -O \
-          | gpg -a --encrypt -o profile.tar.xz.gpg -
-      echo "Saved profile"
-  else
-      echo "No thunderbird profile found at $profile_path"
-  fi
+    local tb_conf_path=~/.thunderbird
+    local profile="zv.default"
+    local profile_path=$tb_conf_path/$profile
+    if [[ -e $profile_path ]]; then
+        echo "Creating profile..."
+        tar --create \
+            --exclude-from=thunderbird_exclude \
+            -C $tb_conf_path $profile \
+            -f temp.tar && \
+            tar -r \
+                -f temp.tar \
+                -C $tb_conf_path \
+                -aJ \
+                $profile/Mail/Feeds/{feeds,feeditems}.rdf \
+                -O \
+            | gpg -a --encrypt -o profile.tar.xz.gpg - && rm temp.tar
+        echo "Appending critical files..."
+        echo "Encrypting"
+        echo "Saved profile"
+    else
+        echo "No thunderbird profile found at $profile_path"
+    fi
 }
 
 backup_profile
