e9ee9475a722bca9c44bb07fc06d3873b0272580
Zephyr Pellerin <zv@nxvr.org>
2016-10-08 19:04:56 -0700

    [emacs] Overhaul all package configuration

--- layers/zv/packages.el
+++ layers/zv/packages.el
@@ -1,7 +1,38 @@
 (defvar zv-packages '(magit
-                      org))
+                      org
+                      edts
+                      erlang
+                      eshell
+                      z3-mode))
 
 (defvar zv-excluded-packages '())
+
+
+(defun zv/post-config-erlang ()
+  (use-package erlang
+    :config
+    (progn
+      (spacemacs/declare-prefix-for-mode 'scheme-mode "mh" "help/show")
+      (spacemacs/declare-prefix-for-mode 'scheme-mode "ms" "repl")
+      (spacemacs/declare-prefix-for-mode 'scheme-mode "mg" "goto")
+
+      (spacemacs/set-leader-keys-for-major-mode 'erlang-mode
+        "hs" 'erlang-show-syntactic-information
+        "si" 'erlang-shell-display
+        "sc" 'erlang-compile-display
+        "ga" 'erlang-beginning-of-clause
+        "gf" 'erlang-beginning-of-function
+        "y" 'erlang-clone-arguments
+        "c" 'erlang-compile))))
+
+(defun zv/init-edts ()
+  (use-package edts
+    :defer t
+    :config
+    (progn
+      (setq-default
+       edts-man-root "/usr/local/lib/erlang")
+      )))
 
 (defun zv/init-bbdb ()
   (use-package bbdb
@@ -18,12 +49,24 @@
         "mf" 'magit-key-mode-popup-gitflow)
       (define-key magit-mode-map "@" 'magit-branch-pull-request))))
 
+
+(defun zv/post-init-eshell ()
+  (with-eval-after-load 'eshell
+    (progn
+      (defalias 'e 'find-file)
+      (defalias 'ff 'find-file)
+      (defalias 'e 'find-file-other-window)
+      (defalias 'gd 'magit-diff-unstaged)
+      (defalias 'gds 'magit-diff-staged))))
+
 
 (defun zv/post-init-org ()
-  (with-eval-after-load 'magit
+  (spacemacs|use-package-add-hook org
+    :post-config
     (progn
       ;; Use our custom org link insertion code
-      (define-key org-mode-map "\C-c\C-l" 'zv/org-insert-link)
+      ;; (define-key org-mode-map "\C-c\C-l" 'zv/org-insert-link)
+      (require 'org-protocol)
 
       (setq-default
        ;; Do not dim blocked tasks
@@ -51,7 +94,9 @@
                                       (tags priority-down category-keep)
                                       (search category-keep)))
 
-       org-todo-keywords '((sequence "TODO" "STARTED" "DONE"))
+       org-log-done 'time
+       org-todo-keywords '((sequence "TODO(t)" "STARTED" "|" "DONE(d!)" "CANCELLED(c@)")
+                           (sequence "READ(r)" "READING" "|" "FINISHED(f!)"))
 
        ;; My shell variant is zsh
        org-babel-sh-command "zsh"
@@ -62,32 +107,26 @@
        org-default-priority ?C
 
        ;; include the emacs diary
-       org-agenda-include-diary t
+       org-agenda-include-diary nil
        ;; Catch edits to invisible sections of the screen
        org-catch-invisible-edits t
 
        ;; Files
-       org-capture-templates `(("t" "Tasks" entry (file+headline ,(concat org-directory "tasks.org") "Tasks")
-                                "* TODO [#A] %?\nSCHEDULED: %t\n")
-                               ;; Quotes
-                               ("k" "Quotes" plain
-                                (file (concat org-directory "quotes.org"))
-                                "#+BEGIN_QUOTE\n%?\n#+END_QUOTE" :empty-lines 1)
-                               ;; Snippets (%x copies from clipboard)
-                               ("s" "Code Snippet" entry (file+headline ,org-default-notes-file "Snippets")
-                                "* %?\n#+BEGIN_SRC %^{prompt}\n%x\n#+END_SRC")
-                               ;; Ideas
-                               ("i" "Ideas" entry
-                                (file+headline ,org-default-notes-file "Ideas")
-                                "* %? %^G\n" :clock-in t :clock-resume t)
-                               ;; awesome list
-                               ("m" "Awesome List" entry
-                                (file+headline ,org-default-notes-file "awesome-list")
-                                "** %?\n\n capture date: %U\n")
-                               ;; Note / Remember
-                               ("n" "Note" entry
-                                (file ,org-default-notes-file)
-                                "* %?\n\n  capture date: %U\n" :empty-lines 1))
+       org-capture-templates
+       `(("a" "Appointment" entry (file+headline ,(concat org-directory "zv.org") "Appointments") "* APPT %^{Description} %^g\n %?\n Added: %U")
+         ("b" "Book/Article" entry (file+headline ,(concat org-directory "zv.org") "Read") "** READ  %?")
+         ;; ("i" "Ideas" entry (file+headline ,(concat org-directory "zv.org") "Ideas") "* %?\nCaptured On: %U\n")
+         ("r" "Refile" entry (file+headline ,org-default-notes-file "Refile") "* %?\nCaptured On: %U\n")
+         ("q" "Quotes" plain (file ,(concat org-directory "quotes.org")) "#+BEGIN_QUOTE\n%?\n#+END_QUOTE" :empty-lines 1)
+         ;; Intentions don't have an active timestamp associated with them, but are marked as TODO items.
+         ("i" "Intentions" entry (file+headline ,(concat org-directory "zv.org") "Tasks") "* TODO %?\nCaptured On: %U\n")
+         ;; Tasks are things that I *need* to get done. They have a clock associated with them and an active timestamp. They appear in the Agenda.
+         ("t" "Tasks" entry (file+headline ,(concat org-directory "zv.org") "Tasks") "* TODO %?\nCaptured On: %T\n"
+          :clock-in t :clock-resume t)
+         ;; Org Protocol
+         ("L" "Protocol Link" entry (file+headline ,org-default-notes-file "Inbox") "* %? [[%:link][%:description]] \nCaptured On: %U")
+         ("P" "Protocol" entry (file+headline ,org-default-notes-file "Inbox") "* %? [[%:link][%:description]] \n#+BEGIN_QUOTE\n%i\n#+END_QUOTE \nCaptured On: %U")
+         ("T" "Thunderbird" entry (file+headline ,org-default-notes-file "Inbox") "* %? %:link\n#+BEGIN_QUOTE\n%:description\n#+END_QUOTE \nCaptured On: %U"))
 
        ;; ORG MODE PUBLISHING
        org-publish-project-alist `(("org-zv"
@@ -106,8 +145,10 @@
                                    ("zv-ghpages" :components ("org-zv")))
 
        ;; Targets include this file and any file contributing to the agenda - up to 6 levels deep
-       org-refile-targets '((org-agenda-files . (:maxlevel . 6))))
-
+       org-refile-targets '((org-agenda-files . (:maxlevel . 6)))
+       org-refile-allow-creating-parent-node t
+       org-refile-use-outline-path t
+       ) ;; end of `set-default'
 
       ;; When running babel blocks in sh, I usually mean `zsh'
       (org-babel-do-load-languages 'org-babel-load-languages
@@ -117,10 +158,6 @@
                                      (python . t)
                                      (ruby . t)
                                      (clojure . t)
-                                     ;; (sh . t)
-                                     ;; (plantuml . t)
-                                     ;; (latex . t)
-                                     (ledger . t)
-                                     ;; (org . t)
+                                     (js . t)
                                      ))
       )))
