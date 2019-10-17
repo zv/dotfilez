4ac200b0cb4896947118acd0d4752021634e31d0
Zephyr Pellerin <zephyr.pellerin@gmail.com>
2019-10-16 23:38:04 -0700

    [emacs] Cleanup custom evil conversions

--- layers/zv/keybindings.el
+++ layers/zv/keybindings.el
@@ -70,14 +70,14 @@
   "D" 'eww-download)
 
 ;; utilities
-(global-set-key (kbd "C-x C-r") 're-builder)
+(zv/set-available-leader "orb" 'regexp-builder)
 ;; applications
 (global-set-key (kbd "<XF86Calculator>") 'calc)
 (global-set-key (kbd "<XF86Mail>") 'gnus)
 
 ;; evil state bindings
 (evil-define-key 'normal evil-surround-mode-map "s" 'evil-surround-region)
-(evil-define-key 'normal evil-surround-mode-map "S" 'evil-substitute)
+;; (evil-define-key 'normal evil-surround-mode-map "S" 'evil-substitute)
 
 ;; (define-key evil-normal-state-map "\C-p" 'helm-projectile-find-file)
 (define-key evil-normal-state-map (kbd "RET") 'evil-scroll-down)
@@ -86,72 +86,60 @@
 ;; insert mode
 (define-key evil-insert-state-map (kbd "C-h") 'backward-char)
 (define-key evil-insert-state-map (kbd "C-l") 'forward-char)
-
-;; mode bindings
-;; Bound to 'goto next line and indent' by default
-(with-eval-after-load 'shell
-  (define-key shell-mode-map "\C-d" nil))
+
 
                                         ; calendar
-(with-eval-after-load 'calendar
-  (setq diary-file (concat org-directory "events"))
-  (add-hook 'calendar-mode-hook
-            (lambda ()
-              (define-key calendar-mode-map "l" 'calendar-forward-day)
-              (define-key calendar-mode-map "h" 'calendar-backward-day)
-              (define-key calendar-mode-map "j" 'calendar-forward-week)
-              (define-key calendar-mode-map "k" 'calendar-backward-week)
-              (define-key calendar-mode-map "{" 'calendar-forward-month)
-              (define-key calendar-mode-map "}" 'calendar-backward-month)
-              (define-key calendar-mode-map "0" 'calendar-beginning-of-week)
-              (define-key calendar-mode-map "$" 'calendar-end-of-week)
-              (define-key calendar-mode-map "[" 'calendar-beginning-of-month)
-              (define-key calendar-mode-map "]" 'calendar-end-of-month)
-              (define-key calendar-mode-map "gg" 'calendar-beginning-of-year)
-              (define-key calendar-mode-map "G" 'calendar-end-of-year))))
-
+(use-package calendar
+  :defer t
+  :bind (:map calendar-mode-map
+         ("l" . calendar-forward-day)
+         ("h" . calendar-backward-day)
+         ("j" . calendar-forward-week)
+         ("k" . calendar-backward-week)
+         ("{" . calendar-forward-month)
+         ("}" . calendar-backward-month)
+         ("0" . calendar-beginning-of-week)
+         ("$" . calendar-end-of-week)))
+
 ;; neotree
-(with-eval-after-load "neotree"
-  (define-key neotree-mode-map "I" 'neotree-hidden-file-toggle)
-  (define-key evil-normal-state-map (kbd "C-\\") 'neotree-find))
+(spacemacs|use-package-add-hook neotree
+  :post-config
+  (progn
+    (define-key neotree-mode-map "I" 'neotree-hidden-file-toggle)
+    (define-key evil-normal-state-map (kbd "C-\\") 'neotree-find)))
 
 (evil-define-key 'motion speedbar-file-key-map
   "l" 'speedbar-expand-line
   "h" 'speedbar-contract-line)
-
-;; Manpages
-(with-eval-after-load "Man"
-  ;; Define our lookup funtion with `K' to be 'man' rather than 'woman'
-  (setq evil-lookup-func #'man)
-  (evil-define-motion evil-lookup ()
-    (call-interactively evil-lookup-func))
-  ;; Format our man pages with a width of 80 chars
-  (setenv "MANWIDTH" "80")
-  (define-key Man-mode-map " "    'scroll-up-command)
-  (define-key Man-mode-map "\177" 'scroll-down-command)
-  (define-key Man-mode-map "}"    'Man-next-section)
-  (define-key Man-mode-map "{"    'Man-previous-section)
-  (define-key Man-mode-map "]" 'evil-forward-paragraph)
-  (define-key Man-mode-map "[" 'evil-backward-paragraph)
-  (define-key Man-mode-map ">"    'end-of-buffer)
-  (define-key Man-mode-map "<"    'beginning-of-buffer)
-  (define-key Man-mode-map "."    'beginning-of-buffer)
-  (define-key Man-mode-map "?"    'evil-search-backward)
-  (define-key Man-mode-map "/"    'evil-search-forward)
-  (define-key Man-mode-map "RET"  'woman-follow)
 
-  (define-key Man-mode-map "n"    'evil-search-next)
-  (define-key Man-mode-map "p"    'evil-search-previous)
-  (define-key Man-mode-map "M-n"  'Man-next-section)
-  (define-key Man-mode-map "M-p"  'Man-previous-section)
+(use-package man
+  :defer t
+  :bind (:map Man-mode-map
+              (" " . scroll-up-command)
+              ("\177" . scroll-down-command)
+              ("}" . Man-next-section)
+              ("{" . Man-previous-section)
+              ("]" . evil-forward-paragraph)
+              ("[" . evil-backward-paragraph)
+              (">" . end-of-buffer)
+              ("<" . beginning-of-buffer)
+              ("." . beginning-of-buffer)
+              ("?" . evil-search-backward)
+              ("/" . evil-search-forward)
+              ("RET" . woman-follow)
 
-  (define-key Man-mode-map "k"    'evil-previous-line)
-  (define-key Man-mode-map "j"    'evil-next-line)
+              ("n" . evil-search-next)
+              ("p" . evil-search-previous)
+              ("M-n" . Man-next-section)
+              ("M-p" . Man-previous-section)
 
-  (define-key Man-mode-map "d"    'scroll-up-command)
-  (define-key Man-mode-map "u"    'scroll-down-command)
-  (define-key Man-mode-map "q"    'Man-kill)
-  (define-key Man-mode-map "m"    'man))
+              ("k" . evil-previous-line)
+              ("j" . evil-next-line)
+
+              ("d" . scroll-up-command)
+              ("u" . scroll-down-command)
+              ("q" . Man-kill)
+              ("m" . man)))
 
 
 ;; Elixir Configuration
@@ -176,31 +164,27 @@
     (define-key helm-ag-map (kbd "<XF86Forward>") 'helm-ag--next-file)
     (define-key helm-ag-map (kbd "<XF86Back>") 'helm-ag--previous-file)))
 
-;; brackets
-(define-key evil-normal-state-map "]" 'evil-forward-paragraph)
-(define-key evil-normal-state-map "[" 'evil-backward-paragraph)
-(define-key evil-motion-state-map "]" 'evil-forward-paragraph)
-(define-key evil-motion-state-map "[" 'evil-backward-paragraph)
-(define-key evil-visual-state-map "]" 'evil-forward-paragraph)
-(define-key evil-visual-state-map "[" 'evil-backward-paragraph)
+;; swap "{" with "[" & "}" with "]"
+(dolist (mode (list evil-normal-state-map evil-motion-state-map evil-visual-state-map))
+  (dolist (pair '(("[" . "{") ("]" . "}")))
+    (let* ((ka (car pair))
+           (kb (cdr pair))
+           (fa (lookup-key mode ka))
+           (fb (lookup-key mode kb)))
+      (define-key mode ka fb)
+      (define-key mode kb fa))))
 
-(define-key evil-normal-state-map "}" 'evil-forward-section-begin)
-(define-key evil-normal-state-map "{" 'evil-backward-section-end)
-(define-key evil-motion-state-map "}" 'evil-forward-section-begin)
-(define-key evil-motion-state-map "{" 'evil-backward-section-end)
-(define-key evil-visual-state-map "}" 'evil-forward-section-begin)
-(define-key evil-visual-state-map "{" 'evil-backward-section-end)
 
 ;; Set emacs as the initial state in a variety of modes
-(mapc (lambda (x) (evil-set-initial-state x 'emacs))
-      '(epa-key-list-mode
-        epa-key-mode
-        epa-mail-mode
-        Info-mode
-        Man-mode
-        term-mode
-        calendar-mode
-        racket-describe-mode))
+(dolist (mode '(epa-key-list-mode
+                epa-key-mode
+                epa-mail-mode
+                Info-mode
+                Man-mode
+                term-mode
+                calendar-mode
+                racket-describe-mode))
+  (evil-set-initial-state mode 'emacs))
 
 ;; Info Mode
 (evil-add-hjkl-bindings Info-mode-map 'emacs
@@ -247,3 +231,13 @@
     ;; Set our custom keybindings
     (spacemacs/set-leader-keys-for-major-mode 'python-mode
       "mv" 'search-parents-for-venv)))
+
+
+(zv//initial-path-keybinding `(("ofez" . ,zv-configuration-layer-directory)
+                               ("ofel" . "~/Development/")
+                               ("ofzd" . "~/dotfilez/")
+                               ("ofod" . ,org-directory)
+                               ("ofoz" . ,zv//org-personal)
+                               ("ofon" . ,org-default-notes-file)
+                               ("ofzb" . ,zv//blog-path)
+                               ("ofzp" . ,(concat zv//blog-path "org/_posts/"))))
